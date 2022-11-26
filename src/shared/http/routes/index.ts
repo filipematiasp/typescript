ceb37c264ce5570151494e0bad2cc705f486aa86
Filipe <filipe.matias@globalbot.com.br>
2022-11-26 15:51:50 -0300

    Novas implementações

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from "express"
+import  productsRouter  from "@modules/products/routes/products.routes"
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    return res.json({ message: 'Hellor dev'})
-})
+router.use('/products', productsRouter)
 
 export default router
