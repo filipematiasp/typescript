53af2a7052b5355c262eaecf9df6163791e2dec2
Filipe <filipe.matias@globalbot.com.br>
2022-11-28 23:46:42 -0300

    navas implementações

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '83eb1d98c40fa260731b30e06177c690',
+        expiresIn: '1d'
+    }
+}
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,13 @@
 import { Router } from "express"
-import  productsRouter  from "@modules/products/routes/products.routes"
+import productsRouter  from "@modules/products/routes/products.routes"
+import usersRouter from "@modules/Users/routes/users.routes"
+import sessionRouter from "@modules/Users/routes/sessions.routes"
 
 const router = Router()
 
 router.use('/products', productsRouter)
+router.use('/users', usersRouter)
+router.use('/sessions', sessionRouter)
+
 
 export default router
