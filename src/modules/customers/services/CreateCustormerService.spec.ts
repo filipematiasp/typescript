f1ee83354500c5620b58bf9059aac23dc1d953b9
Filipe Matias <filipematiasp@gmail.com>
2023-01-12 11:36:37 -0300

    novos testes

--- src/modules/customers/services/CreateCustormerService.spec.ts
+++ src/modules/customers/services/CreateCustormerService.spec.ts
@@ -1,3 +1,39 @@
-test('should be 10', () => {
-    expect(5 + 5).toEqual(10)
+import 'reflect-metadata'
+import CreateCustomerService from './CreateCustormerService'
+import { FakeCustomersRepository } from '../domain/repositories/fakes/FakeCustomersRepository'
+import AppError from '@shared/errors/AppError'
+
+let fakeCustomersRepository: FakeCustomersRepository
+let createCustomer: CreateCustomerService
+
+
+describe('CreateCustomer', () => {
+    beforeEach(() => {
+        fakeCustomersRepository = new FakeCustomersRepository()
+        createCustomer = new CreateCustomerService(fakeCustomersRepository)
+    })
+
+    it('should be able to create a new customer', async () => {
+        const customer = await createCustomer.execute({
+            name: 'Filipe Matias',
+            email: 'filipematias@teste.com'
+        })
+
+        expect(customer).toHaveProperty('id')
+    })
+
+    it('should not be able to create two customers with the same email', async () => {
+        await createCustomer.execute({
+            name: 'Filipe Matias',
+            email: 'filipematias@teste.com'
+        })
+
+        expect(
+            createCustomer.execute({
+                name: 'Filipe Matias',
+                email: 'filipematias@teste.com'
+            })
+        ).rejects.toBeInstanceOf(AppError)
+
+    })
 })
